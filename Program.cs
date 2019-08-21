e9f1f9804593e31abe7d6a37da2999a19d87bda1
Student <example@example.com>
2019-08-21 15:22:14 -0700

    Made Console app and tested calls to api

---
+++ Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+using RestSharp;
+using RestSharp.Authenticators;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using ConsoleApiCall;
+
+using System.Collections.Generic;
+
+namespace ConsoleApiCall
+{
+    public class Article
+    {
+        public string Section { get; set; }
+        public string Title { get; set; }
+        public string Abstract { get; set; }
+        public string Url { get; set; }
+        public string Byline { get; set; }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1
+            var client = new RestClient("https://api.nytimes.com/svc/topstories/v2");
+            // 2
+            var request = new RestRequest("home.json?api-key=" + EnvironmentVariables.ApiKey, Method.GET);
+            // 3
+            var response = new RestResponse();
+
+            // 4
+            Task.Run(async () =>
+            {
+                response = await GetResponseContentAsync(client, request) as RestResponse;
+            }).Wait();
+            JObject jsonResponse = JsonConvert.DeserializeObject<JObject>(response.Content);
+            List<Article> articleList = JsonConvert.DeserializeObject<List<Article>>(jsonResponse["results"].ToString());
+
+            foreach (Article article in articleList)
+            {
+                Console.WriteLine("Section: {0}", article.Section);
+                Console.WriteLine("Title: {0}", article.Title);
+                Console.WriteLine("Abstract: {0}", article.Abstract);
+                Console.WriteLine("Url: {0}", article.Url);
+                Console.WriteLine("Byline: {0}", article.Byline);
+            }
+            Console.ReadLine();
+        }
+
+        // 5
+        public static Task<IRestResponse> GetResponseContentAsync(RestClient theClient, RestRequest theRequest)
+        {
+            var tcs = new TaskCompletionSource<IRestResponse>();
+            theClient.ExecuteAsync(theRequest, response =>
+            {
+                tcs.SetResult(response);
+            });
+            return tcs.Task;
+        }
+    }
+}
